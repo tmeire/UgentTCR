a6c3e783322bee147797e601db5ee431f601473b
Thomas Meire <blackskad@gmail.com>
2010-03-04 19:10:21 +0100

    solved the "tiles of tetris, NOT" problem

---
+++ examples/arna0910-tiles.cpp
@@ -0,0 +1,69 @@
+/*
+ * Tiles (ARNA 2009-2010)
+ * http://acmicpc-live-archive.uva.es/nuevoportal/data/p4734.pdf
+ */
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <cctype>
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <set>
+#include <map>
+#include <list>
+#include <queue>
+#include <vector>
+using namespace std;
+
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define FORD(i,a,b) for(int i=(a);i>=(b);--i)
+// FIXME: FOREACH DOESN'T WORK ON EVERY SYSTEM! Check before contest!
+#define FOREACH(it,c) for(typeof((c).begin()) it=(c).begin();it!=(c).end();++it)
+#define LET(i,c) typeof(c) i = (c)
+#define MP make_pair
+#define PB push_back
+#define SORT(x) sort((x).begin(),(x).end())
+#define ALL(x) (x).begin(),(x).end()
+#define UNIQUE(x) remove(unique((x).begin(),(x).end()),(x).end())
+#define CLEAR(x,v) memset((x),(v),sizeof((x)))
+#define FORS(i,x) for(int i=0;i<(int)(x).size();i++)
+#define X first
+#define Y second
+typedef long long ent;
+typedef pair<int,int> ii;
+
+//Debug info
+#define DEBUG !true
+
+//=========================================END OF HEADER=========================================================
+
+class Class { //edge voor grafen
+	public:
+		bool operator< (const Class& e) const { return 1; };
+};
+
+ent solve (int x, int y) {
+	ent w = x, h = y;
+	while (h != w) {
+		if (h < w) {
+			h += y;
+		} else {
+			w += x;
+		}
+	}
+	return (w / x) * (h / y);
+}
+
+int main () {
+	int x, y;
+	scanf ("%d %d", &x, &y);
+	while (x != 0 && y != 0) {
+		printf("%lld\n", solve (x, y));
+		scanf ("%d %d", &x, &y);
+	}
+	return 0;
+}
